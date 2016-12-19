ae6cab299ad4f65e43e8e15e0d882b87c7db046e
tony-blackie <jim.casey321@gmail.com>
2016-12-19 19:26:53 +0100

    Add root path to avoid duplication

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var path = require('path');
 
 module.exports = {
+    context: path.join(__dirname, 'src'),
     entry: {
-        app: path.join(__dirname, 'src', 'app.js'),
-        about: path.join(__dirname, 'src', 'about', 'about.js')
+        app: path.join('./app.js'),
+        about: path.join('./about/about.js')
     },
     output: {
         path: path.join(__dirname, 'dist'),
